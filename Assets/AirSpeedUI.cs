d5ceba88824bec1e31e540c2a1b6951b3b3ffbe4
Timo <66961498+nuokey@users.noreply.github.com>
2023-02-14 22:14:05 +0300

    Update

---
+++ Assets/AirSpeedUI.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AirSpeedUI : MonoBehaviour
+{
+    public GameObject playerPlane;
+
+
+    void Update()
+    {
+        transform.GetComponent<Text>().text = "Air Speed:" + System.Convert.ToInt32(playerPlane.GetComponent<Plane>().airSpeed * 3.6).ToString() + "km/h";
+    }
+}
